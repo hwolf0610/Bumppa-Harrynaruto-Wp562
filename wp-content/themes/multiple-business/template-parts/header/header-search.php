98c0e412e193d3a80f9fd56c43a40404d7b4f88d
hwolf0610 <hwolf0610@hotmail.com>
2021-03-09 01:37:54 +0100

    deply to github

---
+++ wp-content/themes/multiple-business/template-parts/header/header-search.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Displays header search
+ * @since Multiple Business 1.0.0
+ */
+?>
+
+<div class="header-search-wrap">
+	<button aria-expanded="false" class="search-icon">
+		<span class="kfi kfi-search" aria-hidden="true"></span>
+	</button>
+	<div id="search-form">
+		<?php get_search_form(); ?>
+	</div><!-- /#search-form -->
+</div>
